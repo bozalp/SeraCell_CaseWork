506d0b415697f31350d14cb448f513093a389bd9
Batuhan ÖZALP <batuhan.ozalp.9@yandex.com>
2022-10-29 12:19:17 +0300

    packages added

---
+++ src/Pages/Controller/Automation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
+
+const Automation = () => {
+    return (
+        <View>
+            <Text>
+                automation
+            </Text>
+            <Icon name='home' size={30} color="#000" />
+        </View>
+    )
+}
+
+export default Automation;
---
+++ src/Pages/Controller/OpenClose.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View, } from 'react-native';
+
+const OpenClose = () => {
+    return (
+        <View>
+            <Text>
+                OpenClose
+            </Text>
+        </View>
+    )
+}
+
+export default OpenClose;
